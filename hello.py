c710144c4283068242f2a5e95e083dd86b125da8
DIKSHA SARASWAT <137692643+dikshasaraswa@users.noreply.github.com>
2024-08-15 15:31:50 +0530

    first

--- hello.py
+++ hello.py
@@ -1,1 +1,22 @@
 print("hello world")
+name="diksha"
+age=23
+price=25.99
+print(age)
+# data type in python are = int,float,string, boolean(T and F should be captial), none
+# python is case sensitive and in dbms SQL is not case sensitive and python is implicit typed language, implicit typed language are those in which data type is not defined in front of the variable  * and the second type is explicit typed language(c++,java) in which datatype is defined like (int a=5;)
+#tokens- punctuators,
+
+#expression execution
+# string and numeric can operate with * 
+a,b=2,3
+txt="@"
+print(2*txt*3)
+#string and string can operate with +
+c,d='2',3
+txtt="@"
+print((c+txtt)*3)
+
+#interger division(//) with float and int will give int displayed as float
+#floor gives closest integer,which is lesser than equal to float value, result of (A//B) is same as floor(A/B)
+#remainder(%) is negative when denominator is negative 
