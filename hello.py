64875a4bc78300e50e9551039ed97017759609a2
DIKSHA SARASWAT <137692643+dikshasaraswa@users.noreply.github.com>
2024-08-21 16:01:09 +0530

    first

--- hello.py
+++ hello.py
@@ -43,3 +43,86 @@ a,b=1,int("2")
 sum=a+c
 print(sum)
 
+#\n->nxt line , \t->tab
+
+# slicing 
+# str[starting_idx : ending_idx ] ending idx is not included
+#str[:4] is same as str[0:4], str[1:] is same as str[1: len(str)]
+
+#string functions 
+str.endswith("er.")#returns true if string ends with substr
+str.capitalize() #capital 1st character
+str.replace(old,new) #replaces all occurences of old with new
+str.find(word) #returns 1st idx of the word
+str.count(word) #returns count of word
+
+#lists and tuples 
+# in list we can store element of different type together , in python strings are  immutable but list are mutable
+# immutable : can be accessed using index number but can not be updated ; mutable : can be accessed using index number + update
+#list slicing same as string slicing
+#1. list.append(4) , list.sort() , list.sort(reverse=True),list.reverse(), list.index(idx,el) 3:43:11
+
+#Tuples : a built-in data type  that lets us create immutable sequences of values 
+#tup.index(): returns idx of the first occurrence of element
+
+#dictionary in python
+#stores values in key:value pairs , they are unordered ,mutable(changeable) & dont allow duplicate keys
+#4:24:34
+
+dict = {
+    "name" :"Diksha",
+    "age" : 20,
+    "subjects" :["c","java","c++"],
+    "topic" : ("list","tuple","set"),
+    "address": {
+        "street": "123 Main St",
+        "city": "New York"
+    }
+}
+
+#Set in python : set is the collection of unordered items ,each element in the set is unique & immutable.
+# in set we can add string, numerics, tuple but not list(bcoz list are mutable)
+collection ={1,2,3,"hello","hi"}
+print(collection)
+print(type(collection))
+print(len(collection)) #total no. of diff. values
+coll = set() #empty set coll={} this is a empty dictionary 
+coll.add(1)
+coll.add(2)
+coll.remove(1)
+
+#loops 
+#break : used to terminate the loop when encountered
+#continue: terminates execution in the current iteration & contiues execution of the loop with the next iteration 
+#pass is a null statement that does nothing .it is used as a placeholder for future work.(5:56:49)
+
+# functions in Python : Block of statements that perform a task , functions reduces redancy in code 
+def cal_sum(a,b):
+    return a+b
+sum = cal_sum(4,5) # function call
+print(sum)
+
+# both lines to be printed in a single line 
+print("hello", end ="") #sep=" "
+print("world") #end ="\n"
+
+def cal_prod(a=1,b=1):#default values , (a=1,b) is not valid ,(a,b=1) is valid , default value are given from last
+    print(a*b)
+    return a*b
+cal_prod()
+
+#recursion : when a function calls itself repeatedly
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+print(factorial(5))
+
+def show(n):
+    if(n==0):
+        return 
+    print(n)
+    show(n-1)
+    print("end")
+    
