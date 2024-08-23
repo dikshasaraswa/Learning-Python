e550a1a7e273d1a7e6228478fd6e36d40fe417f2
DIKSHA SARASWAT <137692643+dikshasaraswa@users.noreply.github.com>
2024-08-24 00:49:31 +0530

    f

---
+++ OOP.py
@@ -0,0 +1,10 @@
+#we started using objects in code
+#class is a blueprint for creating objects
+class Student:
+    def __init__(self): #constructor function
+         print("adding new student")
+    name="aman"
+s1 = Student()#obj = instance
+print(s1.name) # accessing the attribute of the object
+
+#init function?contsructor function 
